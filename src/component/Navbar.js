c18965d18c99c8c7efa09a47d67fd98258a11263
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-03-30 00:09:23 +0900

    7. 세번째 프로젝트: 쇼핑몰 웹사이트 만들기  10강~13강

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 // import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 // import { fas, far, fal } from "@awesome.me/kit-KIT_CODE/icons";
 import {
   faMagnifyingGlass,
   faSignOutAlt,
 } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 // mouse change pointer
+
+// 키를 입력했을 때 이벤트를 호출시킬 수 있는 > onKeyPress
+// 언제 생성되냐? 내가 키를 입력하면 생성 됨
+// onKeyPress 발생한다 ? 언제 ? 유저가 키를 눌렀을 때
+// not fire alt ctrl esc 에 대해서는 동작하지 않는다. > 그 외에 키는 동작한다.
+// 이벤트 함수를 자동으로 보내준다.
+// 그러면 search가 이벤트라는 얘를 받게된다.
+
+// !! 모든 이벤트 리스너는 이벤트를 매개변수로 넘겨줌
+
+// 그러면 받은 이벤트를 가지고 우리는 뭘 할것이냐
+
+// 만약에 이벤트에 있는 key가 Enter이다?
+// 그러면 콘솔로그를 체크하게 해봄
 
 function Navbar({ isAuthenticated, setAuthenticate }) {
   const menuList = [
@@ -28,6 +43,27 @@ function Navbar({ isAuthenticated, setAuthenticate }) {
 
   const navigate = useNavigate();
 
+  // 사이드 메뉴
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const toggleSideMenu = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
+  //   const toggleSideMenu = () => {
+  //     if (isSideMenuOpen) {
+  //       setIsSideMenuOpen(false); // 사이드 메뉴가 보인다면 숨깁니다.
+  //     } else {
+  //       setIsSideMenuOpen(true); // 사이드 메뉴가 숨겨져 있다면 보이게 합니다.
+  //     }
+  //   };
+
+  //   const toggleSideMenu = () => {
+  //     // 화면 너비가 768px 이하인 경우에만 사이드 메뉴 토글
+  //     if (window.innerWidth < 768) {
+  //       setIsSideMenuOpen(!isSideMenuOpen);
+  //     }
+  //   };
+
   // 로그인페이지 이동
   const goToLogin = () => {
     navigate("/login");
@@ -38,26 +74,77 @@ function Navbar({ isAuthenticated, setAuthenticate }) {
     navigate("/login");
   };
 
+  // 자바스크립트는 value에 읽어오고싶은 값이 들었지만
+  // 리액트는 조금 다른다. 우리가 읽어오고 싶은값이 이벤트애 들어가 있다
+  // let keyword = >>>> e.target.value; 이 벨류안에 우리가 입력한 키워드가 있다.
+
+  // 우리가 input안에 있는 값을 읽어오고 싶을애는 저번에 자바스크립트처럼 value 쓰는게 아니고
+
+  // 리액트
+  // e.target.value; 사용하면 이 인풋안에 있는 값을 읽어올 수 있다.
+  // 외워둬라
+  //
+
+  // 파라미터 값이 아니라 쿼리값이다.
+  //  ?q=검색어
+
+  // 어떤 값이 들어갈 것이냐? 우리가 이번에 할것은 쿼리이다.
+  // 파라미터 값이 아닌 쿼리값을 우리는 서치하는 것이다.
+  // ?q=검색어
+  // 추가적인 조건에 대해서는 쿼리를 붙인디ㅏ.
+
+  // 추가적인 조건
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      console.log("We click this key", e.key);
+      // 입력한 검색어를 읽어와서
+      let keyword = e.target.value;
+      console.log("keyword", keyword);
+      // console.log("onKeyDown");
+
+      // url을 바꿔준다.(Url뒤에 파라미터 추가하며 됨) >> navogafe
+      navigate(`/?q=${keyword}`);
+      // 어떤 데이터와 갈거냐하면, 큐는 바로 쿼리값을 받아와야 한다.
+      // 키워드 읽어와서, 그것을 url에 넣어주는것까지 우리가 했어
+      // 넣어주면 뭐할꺼야, 검색이 안되는데
+      // 검색기능을 어떻게 해줄것이냐면,
+      // 쿼리에 있는 글자를 읽어와서
+      // 이키워드를 가진얘가 있는지 다시 검사를 해준다. >> 이부분 해보자 >> ProductAll로 이동해서 해보잡
+    }
+    // console.log("onKeyDown");
+  };
+
+  // 쿼리에 있는 글자를 읽어와서 검색을 해준다.
+
   return (
     <>
-      {isAuthenticated ? (
-        <div className="login-button" onClick={logoutUser}>
-          <FontAwesomeIcon icon={faSignOutAlt} size="2x" />
-          <div className="login-txt-style">Logout</div>
-        </div>
-      ) : (
-        <div className="login-button" onClick={goToLogin}>
-          <FontAwesomeIcon icon={faUser} size="2x" />
-          <div className="login-txt-style">Login</div>
-        </div>
-      )}
+      {/* 사이드메뉴 및 버튼 */}
+      <div className="mobile-top">
+        <button className="menu-toggle" onClick={toggleSideMenu}>
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </button>
+        {isAuthenticated ? (
+          <div className="login-button" onClick={logoutUser}>
+            <FontAwesomeIcon icon={faSignOutAlt} size="2x" />
+            <div className="login-txt-style">Logout</div>
+          </div>
+        ) : (
+          <div className="login-button" onClick={goToLogin}>
+            <FontAwesomeIcon icon={faUser} size="2x" />
+            <div className="login-txt-style">Login</div>
+          </div>
+        )}
+      </div>
 
       <div className="nav-section">
-        <img
-          width={100}
-          src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1968-1999.png"
-          alt="로고"
-        />
+        <Link to="/">
+          <img
+            width={100}
+            src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1968-1999.png"
+            alt="로고"
+          />
+        </Link>
       </div>
       <div className="menu-area">
         {/* <ul>
@@ -66,11 +153,22 @@ function Navbar({ isAuthenticated, setAuthenticate }) {
           ))}
         </ul> */}
         <div className="binBox"></div>
-        <ul className="menu-list">
+        {/* {isSideMenuOpen && ( */}
+        <ul
+          // className="menu-list"
+          className={`menu-list ${isSideMenuOpen ? "open" : ""}`}
+          // style={{ display: isSideMenuOpen ? "block" : "none" }}
+        >
           {menuList.map((menu) => (
             <li>{menu}</li>
           ))}
         </ul>
+        {/* )} */}
+        {/* <ul>
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul> */}
         <div className="search">
           <button className="search-icon-sx">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
@@ -82,7 +180,9 @@ function Navbar({ isAuthenticated, setAuthenticate }) {
             // id="icons-search"
             // name="icons-search"
             // class="rounded"
-            class="border-bottom"
+            className="border-bottom"
+            onKeyDown={(e) => search(e)}
+            // onKeyPress={}
           />
         </div>
         {/* <div>
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,6 +5,30 @@ import { useSearchParams } from "react-router-dom";
 
 // 구글링 my json server
 
+// 쿼리가 있으면!!(키워드가 있으면) 쿼리를 붙여서 데이터 검색하기
+
+// url에 있는 쿼리값을 읽어옵시다.
+// !!! UseSearchParams라는 리액트 훅을 사용
+// 약간의 useState처럼 사용된다.
+// 쿼리안에는 query.get 사용하게 되면 내가 원하는 것을 읽어 올 수 있다.
+
+// q라고 시작되는 것에 아이템을 가져다가 쿼리에 넣어줄 것이다.
+
+// 앞으로 getProducts를 통해서 api함수를 호출할쩍에, 그냥 api 호출을 하는게 아니라, 쿼리에 있는 값을 넣어줄 것이다. ?q=${searchQuery}
+// !! 만약에 Query값이 없는 경우에는 디폴트, 아무것도 없는 빈스트링 넣어줄게요
+
+// !!! 한번실행되고 나서 검색해도 실행안된다 왜그럴까 ? 중요 한것 여기서 !!!
+// useEffect는 언제 한번 콜이된다?
+// 프로젝트 시작후 딱 한번 ( 실행할때 딱한번 호출)
+// 이 getProducts를 언제다시 호출해줘야 함?
+// query 쿼리 값이 바뀔 때 마다, 이함수가 호출되야 한다.
+// 헤이 쿼리가 바뀌면 너도 다시 호출을 해줄래?
+
+// ?? 이거 서치를 어떻게 하는 건가요? 내가 서치알고리즘을 만든것도 아니고 어떻게 큐하면 서치가 되는것이지?
+// 이거는 제이슨서버에서 알아서 자동으로, 쿼리를 보내주면, 알아서, 자동으로 서치를 하게된다. ?q=${searchQuery}
+// 서치하는 알고리즘에 대해서는 걱정할게 없다. 제이슨 서버에서 알아서 해주니깐
+// 우리가 해줄게 뭐다? 서치해줄 큐 키워드를 넣어주면 알아서 해준다잉
+
 function ProductAll() {
   const [productList, setProductList] = useState([]);
   let [query, setQuery] = useSearchParams();
@@ -12,6 +36,7 @@ function ProductAll() {
   const getProducts = async () => {
     // let url = "http://localhost:5000/products";
     let searchQuery = query.get("q") || "";
+    console.log("### searchQuery 쿼리값은?", searchQuery);
     let url = `https://my-json-server.typicode.com/hey-anna/hnm-react-router-practice/products?q=${searchQuery}`;
 
     // let response = await fetch(url);
@@ -35,7 +60,9 @@ function ProductAll() {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
+  // 헤이 쿼리가 바뀌면 너도 다시 호출을 해줄래?
+
   return (
     <>
       <div className="card-box-style">
