d8dc37eb83b4ddf6a46bdda2c58f067bb6a37791
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-02 08:59:48 +0900

    리덕스 세팅 환경설정 완료

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
+// Provider : store를 app에 적용시키는 컴포넌트
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/redux/reducer/productReducer.js
@@ -0,0 +1,7 @@
+// 객체
+let initialState = {};
+
+// 리듀서는 함수
+function productReducer(state = initialState, action) {}
+
+export default productReducer;
